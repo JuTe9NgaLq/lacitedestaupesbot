5f49f7f112cc7bd04a0769969bc99dabc321d02e
JuTe9NgaLq <40941404+JuTe9NgaLq@users.noreply.github.com>
2018-08-28 22:23:22 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -1,6 +1,6 @@
 const Discord = require('discord.js');
 const bot = new Discord.Client();
-bot.login('NDgzNzQzNDIyNjM3NjcwNDEx.DmX_5g.5jWGSYwTiBrqJqsR19_jnYBMaL8');
+bot.login(process.env.TOKEN);
 
 bot.on('message', message => {
 	if (message.content != '<@368050653118988292> :eyes:  :fire: :100: :fire: Mathraining :100: :fire: :100: :eyes:'){
