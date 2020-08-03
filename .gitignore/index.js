247ff1982aa2419c3f279b345c954b1e2ba51bf4
JuTe9NgaLq <40941404+JuTe9NgaLq@users.noreply.github.com>
2020-08-03 15:00:04 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -3,9 +3,9 @@ const bot = new Discord.Client();
 bot.login(process.env.TOKEN);
 
 bot.on('message', message => {
-	if (message.content != '<@368050653118988292> :eyes:  :fire: :100: :fire: Mathraining :100: :fire: :100: :eyes:'){
+	if (message.content != '<@196705023772721153> :eyes:  :fire: :100: :fire: Mathraining :100: :fire: :100: :eyes:'){
 		msg = message.content.toLowerCase();
     if (msg.includes('mathraining') || msg.includes(' mt') || msg.includes('mt ') || msg.includes('matrain')){
-    message.channel.sendMessage('<@368050653118988292> :eyes:  :fire: :100: :fire: Mathraining :100: :fire: :100: :eyes:');
+    message.channel.sendMessage('<@196705023772721153> :eyes:  :fire: :100: :fire: Mathraining :100: :fire: :100: :eyes:');
 	}}
 });
