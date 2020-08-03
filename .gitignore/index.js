eb6bf9be29b6676a9e120bbef427c2b304799b46
JuTe9NgaLq <40941404+JuTe9NgaLq@users.noreply.github.com>
2020-08-03 15:29:20 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -8,4 +8,12 @@ bot.on('message', message => {
     if (msg.includes('mathraining') || msg.includes(' mt') || msg.includes('mt ') || msg.includes('matrain')){
     message.channel.sendMessage('<@196705023772721153> :eyes:  :fire: :100: :fire: Mathraining :100: :fire: :100: :eyes:');
 	}}
+	
+	if (message.content != '<@228755657766076416> :nerd: Le poly :nerd: <@211918230376153090>'){
+		msg = message.content.toLowerCase();
+    if (msg.includes(' poly ')){
+    message.channel.sendMessage('<@228755657766076416> :nerd: Le poly :nerd: <@211918230376153090>'');
+	}}
+	
+	
 });
